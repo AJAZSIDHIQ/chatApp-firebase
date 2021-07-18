ee8fefc0688c10260c356e9657e5ebec731b837f
ajazsidhiq <ajaz.s@zohocorp.com>
2021-07-18 19:34:01 +0530

    final touch

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ var isCurrentUserOnline = null
 
 var currentChatRef = null
 var currentchatid = null
-var chatuserid = null;
+var currentchatuserid = null
+
+//var chatuserid = null;
 var chatusername = null;
+
+var friendsChatListners = {}
 
 
 
@@ -28,11 +32,22 @@ var chatusername = null;
 
    currentUserRef = firebase.database().ref('users/' + user.uid)
 
-   currentUserRef.set({
-    username: user.displayName,
-    email: user.email,
-    isOnline : true
-  });
+   currentUserRef.get().then((snapshot) => {
+        if (snapshot.exists()) {
+            makeOnline()
+        } else {
+            currentUserRef.set({
+                username: user.displayName,
+                email: user.email,
+                isOnline : true
+            });
+            isCurrentUserOnline = true
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+
+
 
   isCurrentUserOnline = true
 
@@ -49,18 +64,37 @@ var chatusername = null;
 
 
 function clickforChat(chatuserId){
+    currentchatuserid = chatuserId
     document.getElementById("info-panel").style.display= "none"
     document.getElementById("right-panel").style.display= "block"
-    database.ref().child("users").child(chatuserId).get().then((snapshot) => {
+
+    var chatuserIdref  = database.ref().child("users").child(chatuserId)
+
+    //checking whether chat is linkned to user
+    currentUserRef.child("friends").child(chatuserId).get().then((snapshot) => {
+        if (!snapshot.exists()) {
+            currentUserRef.child("friends").child(chatuserId).set(true);
+            chatuserIdref.child("friends").child(currentUid).set(true);
+        } 
+    }).catch((error) => {
+        console.error(error);
+    });
+
+    //finding username 
+    chatuserIdref.once('value', (snapshot) => {
         if (snapshot.exists()) {
             //console.log(snapshot.val());
             chatusername = snapshot.val().username
+            document.getElementById("chat-header").innerText ="messages : " + chatusername
         } else {
             console.log("No data available");
         }
     }).catch((error) => {
         console.error(error);
     });
+
+
+
     var chatid = "" 
     if(currentUid < chatuserId) 
         chatid = currentUid + chatuserId
@@ -80,16 +114,18 @@ function clickforChat(chatuserId){
     currentChatRef.once('value', (snapshot) => {
         snapshot.forEach((childSnapshot) => {
             var msgJson = childSnapshot.val();
+            var msgkey = childSnapshot.key
             // ...
             if(msgJson.sender == currentUid){
                 htmlContent += "<div id='chat-"+childSnapshot.key+"' class='container'><p><b> ME </b>: "+msgJson.msg+"</p><span class='time-right'>"+ new Date(msgJson.timestamp * 1000).toLocaleString()+"</span>"
 
+                if (msgJson.deliveredAt){
+                    htmlContent += "<span id='delivered-"+msgkey+"' class='time-left' title='delivered at : "+ new Date(msgJson.deliveredAt * 1000).toLocaleString()+"'>delivered</span><br>"
+                }
                 if(msgJson.seenAt) {
-                    htmlContent += "<span class='time-left' title='seen at : "+new Date(msgJson.seenAt * 1000).toLocaleString() +" \n delivered at : "+ new Date(msgJson.deliveredAt * 1000).toLocaleString()+"'>seen</span>"
+                    htmlContent += "<span id='seen-"+msgkey+"' class='time-left' title='seen at : "+new Date(msgJson.seenAt * 1000).toLocaleString() +"'>seen</span>"
                 }
-                else if (msgJson.deliveredAt){
-                    htmlContent += "<span class='time-left' title='delivered at : "+ new Date(msgJson.deliveredAt * 1000).toLocaleString()+"'>delivered</span>"
-                }
+
             }
 
             else{
@@ -111,48 +147,50 @@ function clickforChat(chatuserId){
 
         document.getElementById("message-container").innerHTML =htmlContent
 
+        document.getElementById("user-"+chatuserId+"-unread").innerText =""
+
         window.scrollTo(0,document.body.scrollHeight);
     });
 
-    currentChatRef.on('child_added', (data) => {
-        var msgJson = data.val();
-            // ...
-        let el = document.createElement('div');
-        if(!document.getElementById("chat-" + data.key)){
-            if(msgJson.sender == currentUid){
-                el.innerHTML = "<div id='chat-"+data.key+"'  class='container'><p><b> ME </b>: "+msgJson.msg+"</p><span class='time-right'>"+ new Date(msgJson.timestamp * 1000).toUTCString()+"</span></div>"
-            }
+    // currentChatRef.on('child_added', (data) => {
+    //     var msgJson = data.val();
+    //         // ...
+    //     let el = document.createElement('div');
+    //     if(!document.getElementById("chat-" + data.key)){
+    //         if(msgJson.sender == currentUid){
+    //             el.innerHTML = "<div id='chat-"+data.key+"'  class='container'><p><b> ME </b>: "+msgJson.msg+"</p><span class='time-right'>"+ new Date(msgJson.timestamp * 1000).toUTCString()+"</span></div>"
+    //         }
     
-            else{
-                el.innerHTML = "<div id='chat-"+data.key+"' class='container darker'><p><b> "+chatusername+"</b> : "+msgJson.msg+"</p><span class='time-left'>"+ new Date(msgJson.timestamp * 1000).toUTCString()+"</span></div>"
-            }
-            document.getElementById("message-container").append(el)
-        }
+    //         else{
+    //             el.innerHTML = "<div id='chat-"+data.key+"' class='container darker'><p><b> "+chatusername+"</b> : "+msgJson.msg+"</p><span class='time-left'>"+ new Date(msgJson.timestamp * 1000).toUTCString()+"</span></div>"
+    //         }
+    //         document.getElementById("message-container").append(el)
+    //         window.scrollTo(0,document.body.scrollHeight);
+    //     }
 
-
-        window.scrollTo(0,document.body.scrollHeight);
-
-        if(msgJson.sender != currentUid  && !msgJson.seenAt){
-            let updateJson ={}
-            if(!msgJson.deliveredAt)
-                updateJson.deliveredAt = firebase.database.ServerValue.TIMESTAMP
-            updateJson.seenAt = firebase.database.ServerValue.TIMESTAMP
-            currentChatRef.child(data.key).update(updateJson);
-        }
-    });
+    //     if(msgJson.sender != currentUid  && !msgJson.seenAt){
+    //         let updateJson ={}
+    //         if(!msgJson.deliveredAt)
+    //             updateJson.deliveredAt = firebase.database.ServerValue.TIMESTAMP
+    //         updateJson.seenAt = firebase.database.ServerValue.TIMESTAMP
+    //         currentChatRef.child(data.key).update(updateJson);
+    //     }
+    // });
 
     currentChatRef.on('child_changed', (data) => {
         var msgJson = data.val();
+        var chatkey = data.key
             // ...
         let el = document.getElementById('chat-'+data.key);
-        if(el && msgJson.sender == currentUid && !(el.childElementCount>2) && (msgJson.seenAt || msgJson.deliveredAt)) {
+        if(el && msgJson.sender == currentUid  && (msgJson.seenAt || msgJson.deliveredAt)) {
             let temp = document.createElement('div');
-            if(msgJson.seenAt) {
-                temp.innerHTML = "<span class='time-left' title='seen at : "+new Date(msgJson.seenAt * 1000).toLocaleString() +" \n delivered at : "+ new Date(msgJson.deliveredAt * 1000).toLocaleString()+"'>seen</span>"
+            if (msgJson.deliveredAt && !document.getElementById('delivered-'+chatkey)){
+                temp.innerHTML = "<span id='delivered-"+chatkey+"' class='time-left' title='delivered at : "+ new Date(msgJson.deliveredAt * 1000).toLocaleString()+"'>delivered</span><br>"
             }
-            else if (msgJson.deliveredAt){
-                temp.innerHTML = "<span class='time-left' title='delivered at : "+ new Date(msgJson.deliveredAt * 1000).toLocaleString()+"'>delivered</span>"
+            if(msgJson.seenAt && !document.getElementById('seen-'+chatkey)) {
+                temp.innerHTML += "<span id='seen-"+chatkey+"' class='time-left' title='seen at : "+new Date(msgJson.seenAt * 1000).toLocaleString() +"'>seen</span>"
             }
+
             el.append(temp)
         }
 
@@ -162,7 +200,7 @@ function clickforChat(chatuserId){
  function sendmsg() {
      let inputelem = document.getElementById("message-input")
      let inputmsg =  inputelem.value
-     console.log(inputmsg)
+    //  console.log(inputmsg)
 
      var newMsg = currentChatRef.push();
      newMsg.set({
@@ -174,26 +212,109 @@ function clickforChat(chatuserId){
         });
     inputelem.value = ''
  }
-
  userRef.once('value', (snapshot) => {
-        console.log(snapshot.val());
+        // console.log(snapshot.val());
         let users = snapshot.val()
         let htmlContent = ""
         for (const key in users) {
-            if(key == currentUid)
+            if(key == currentUid){
+                for(const frienduserid in users[key].friends){
+                    checkForUnreadMsg(frienduserid)
+                }
+                database.ref('users/' + currentUid +"/friends").on('child_added', (data) => {
+
+                    var newuserid = data.key;
+                    // console.log(newuserid)
+                    checkForUnreadMsg(newuserid);
+                });
                 continue;
+            }
 
             let isOnline = "offline"
             if(users[key].isOnline == true)
                 isOnline = "online"
 
             //console.log(`${key}: ${user[key]}`);
-            htmlContent += "<li id='user-"+key +"' class='w3-bar' onclick=\"clickforChat('"+key+"')\"><div class='w3-bar-item'><span class='w3-large'>"+users[key].username+"</span><br><span>"+isOnline+"</span></div></li>"
+            htmlContent += "<li id='user-"+key +"' class='w3-bar' onclick=\"clickforChat('"+key+"')\"><div class='w3-bar-item'><span class='w3-large'>"+users[key].username+"</span><br><span id = 'user-"+key+"-status'>"+isOnline+"</span> <br> <div id = 'user-"+key+"-unread'></div></div></li>"
         }
         document.getElementById("users-list-ul").innerHTML = htmlContent
     }, (errorObject) => {
         console.log('The read failed: ' + errorObject.name);
     });
+
+    // friendslist.forEach( (frienduserid) => {
+    //     console.log(frienduserid)
+    // })
+
+    function checkForUnreadMsg(frienduserid) {
+        var chatid = findChatid(currentUid ,frienduserid)
+        database.ref('chats/'+chatid).once('value', (snapshot) => {
+
+            snapshot.forEach((childSnapshot) => {
+                var msgid = childSnapshot.key;
+                var msgJson = childSnapshot.val();
+                if(!msgJson.deliveredAt){
+                    database.ref('chats/'+chatid +"/"+ msgid ).child('deliveredAt').set(firebase.database.ServerValue.TIMESTAMP);                   
+                }
+                if(!msgJson.seenAt && msgJson.sender != currentUid){
+                    let el = document.getElementById('user-'+frienduserid+'-unread')
+                    if(el){
+                        el.innerText ="unread chat"
+                    }
+                }
+
+            });         
+
+            //let lastmsgid = Object.keys(snapshot.val()).pop()
+
+            // database.ref('chats/'+chatid +"/"+ lastmsgid ).once('value', (snapshot) => {
+            //     if(snapshot.val() && snapshot.val().sender != currentUid && !snapshot.val().seenAt){
+
+            //     }
+            // });
+
+
+        });
+
+        friendsChatListners[frienduserid] = database.ref('chats/'+chatid).on('child_added', (data) => {
+            var msgJson = data.val();
+                // ...
+            let el = document.createElement('div');
+            if((currentchatuserid == msgJson.sender ||  msgJson.sender ==  currentUid )&& !document.getElementById("chat-" + data.key)){
+                if(msgJson.sender == currentUid){
+                    el.innerHTML = "<div id='chat-"+data.key+"'  class='container'><p><b> ME </b>: "+msgJson.msg+"</p><span class='time-right'>"+ new Date(msgJson.timestamp * 1000).toUTCString()+"</span></div>"
+                }
+        
+                else{
+                    el.innerHTML = "<div id='chat-"+data.key+"' class='container darker'><p><b> "+chatusername+"</b> : "+msgJson.msg+"</p><span class='time-left'>"+ new Date(msgJson.timestamp * 1000).toUTCString()+"</span></div>"
+                }
+                document.getElementById("message-container").append(el)
+                window.scrollTo(0,document.body.scrollHeight);
+                if(msgJson.sender != currentUid  && !msgJson.seenAt){
+                    let updateJson ={}
+                    if(!msgJson.deliveredAt)
+                        updateJson.deliveredAt = firebase.database.ServerValue.TIMESTAMP
+                    updateJson.seenAt = firebase.database.ServerValue.TIMESTAMP
+                    currentChatRef.child(data.key).update(updateJson);
+                }
+            }
+            else{
+                let el = document.getElementById('user-'+frienduserid+'-unread')
+                if(el && !msgJson.seenAt && msgJson.sender != currentUid ){
+                    el.innerText ="unread chat"
+                }
+
+                if( msgJson.sender != currentUid  && !msgJson.deliveredAt){
+                    database.ref('chats/'+chatid +"/"+ data.key ).child('deliveredAt').set(firebase.database.ServerValue.TIMESTAMP);                   
+                }
+            }
+    
+
+        });
+
+    }
+
+
 
     userRef.on('child_added', (data) => {
         var msgJson = data.val();
@@ -203,7 +324,7 @@ function clickforChat(chatuserId){
             if(msgJson.isOnline == true)
                 isOnline = "online"
         let el = document.createElement('div');
-        el.innerHTML = "<li id='user-"+data.key +"' class='w3-bar' onclick=\"clickforChat('"+data.key +"')\"><div class='w3-bar-item'><span class='w3-large'>"+msgJson.username+"</span><br><span>"+isOnline+"</span></div></li>"
+        el.innerHTML = "<li id='user-"+data.key +"' class='w3-bar' onclick=\"clickforChat('"+data.key +"')\"><div class='w3-bar-item'><span class='w3-large'>"+msgJson.username+"</span><br><span id = 'user-"+data.key+"-status'>"+isOnline+"</span><br> <div id = 'user-"+data.key+"-unread'></div></div></li>"
 
         document.getElementById("users-list-ul").append(el)
 
@@ -219,12 +340,30 @@ function clickforChat(chatuserId){
             let isOnline = "offline"
             if(user.isOnline == true)
                 isOnline = "online"
-        let el = document.getElementById('user-'+userid);
-        htmlContent = "<div class='w3-bar-item'><span class='w3-large'>"+user.username+"</span><br><span>"+isOnline+"</span></div>"
+        let el = document.getElementById('user-'+userid+'-status');
+        //htmlContent = "<div class='w3-bar-item'><span class='w3-large'>"+user.username+"</span><br><span>"+isOnline+"</span></div>"
 
-        el.innerHTML = htmlContent
+        el.innerHTML = isOnline
 
     });
+
+
+    function addmessageListner( frienduserid) {
+        var chatid = "" 
+        if(currentUid < frienduserid) 
+            chatid = currentUid + frienduserid
+        else 
+            chatid = frienduserid + currentUid
+        
+        
+    }
+
+    function findChatid (userid1 , userid2) {
+        if(userid1 < userid2) 
+            return userid1 + userid2
+        else 
+            return  userid2 + userid1
+    }
 
 
     function signOut() {
@@ -260,7 +399,7 @@ function clickforChat(chatuserId){
 
 
 
-    var offlineTime = 10000;
+    var offlineTime = 20000;
     var offlineTTimer = setTimeout(makeOffline, offlineTime);
 
     var resetTimer = function() {
